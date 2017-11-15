0cf2aba81e755be7bf0e31fbb349ec1b2422f7a6
Mysdreivus <ruel.gordon@bison.howard.edu>
2017-11-14 19:24:54 -0500

    Add files via upload

--- HomeWork1.asm
+++ HomeWork1.asm
@@ -1,17 +1,26 @@
 .data
-	mystring:	.asciiz "This is the 8 or less character text: "
+	mystring:	.asciiz "Enter the 8 character code: "
+	
+	# Allocates space for 8 characters
+	
 	userInput:	.space 8
+	finalstring:	.asciiz "The string is: "
 .text
 	main:
+		# Displays Hello String
+		li $v0, 4
+		la $a0, mystring
+		syscall
+		
 		# Getting user's input as text
 		li $v0, 8
 		la $a0, userInput
 		li $a1, 8
 		syscall
 		
-		# Displays Hello String
+		# Displays the final string
 		li $v0, 4
-		la $a0, mystring
+		la $a0, finalstring
 		syscall
 		
 		# Displays the name
