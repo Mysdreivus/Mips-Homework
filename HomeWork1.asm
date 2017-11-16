afed2d82bbe6dda66ebc6a30d134722d6487c004
Mysdreivus <ruel.gordon@bison.howard.edu>
2017-11-15 23:44:09 -0500

    Add files via upload

--- HomeWork1.asm
+++ HomeWork1.asm
@@ -28,7 +28,7 @@ main:
 
        la $a0, ($t0)             #load address of user_input in $t0 to argument register $a0
 
-       la $a1, 9                 #gets length of space in myWord to avoid exceeding memory limit
+       la $a1, 9                 #gets length of space in Word to avoid exceeding memory limit
 
        syscall                   #syscall to read user_input and store the string in memory
 
@@ -41,27 +41,48 @@ main:
 checklength:
 
        lb $t1, 0($s5)
-
+	   
        beq $t1, 0, subtractfour
 
        beq $t1, 10, subtractfour
+	   
+	   bne $t1, 32, Increment
 
-       addi $s3, $s3, 4
+offsetthing: 
+
+		addi $s5, $s5, 1
+		
+		j checklength
+		
+Increment:
+		
+	   addi $s3, $s3, 4
 
        addi $s5, $s5, 1
-
+	   
        j checklength
 
 subtractfour:
 
-       addi $s3, $s3, -4			#Remove extra 4 from
+       addi $s3, $s3, -4		   #Remove extra 4 from
+	   j iterate_string
 
+firstchar:
+
+		addi $t3, $0, 1
+		lb $t1, 0($t0)
+		bne $t1, 10, iterate_string
+		beq $t1, 10, invalid
+		
 iterate_string:                     #iterate through input to check if all the charectars are valid    
 
-      
-
+		
        lb $t1, 0($t0)               #first byte of memory into register $t1
-
+	   
+	   beq $t3, 0, firstchar
+	   
+	   beq $t1, 32, isspace			#if the value is a space branch to isspace and offset by 1
+	   
        beq $t1, 0, less_than_ten
 
        beq $t1, 10, less_than_ten
@@ -86,8 +107,16 @@ iterate_string:                     #iterate through input to check if all the c
 
        bgt $t1, 102, invalid     #branch to invalid if value in $t1 is greater than 102 (ASCII dec for 'f')
 
-      
 
+
+		
+isspace:
+
+		addi $t0, $t0, 1	
+		addi $s3, $s3, -4
+		bne $t0, $t7, iterate_string
+
+		
 invalid:                      #label to call invalid and exit program
 
        li $v0, 4
@@ -100,35 +129,32 @@ invalid:                      #label to call invalid and exit program
 
        syscall
 
-valid:                             #label to call valid and exit program
-
-       addi $t0, $t0, 1            #increment offset of $t0 by 1
-
+valid:                             	   #label to call valid and exit program
+	   
+	
+	   addi $t3, $0, 1
+       addi $t0, $t0, 1                #increment offset of $t0 by 1
        sub $s4, $t1, $s1
-
        sllv $s4, $s4, $s3
-
        addi $s3, $s3, -4
-
        add $s2, $s4, $s2
-
-       bne $t0, $t7, iterate_string    #if offset of $t0 is not equal to $t7, branch to iterate_string to continue looping
+       bne $t0, $t7, iterate_string    #if offset of $t0 is not equal to $t7, branch to iterate string
 
  
 
 less_than_ten:
-
+	   
        addi $s0, $0, 10
 
        addi $t0, $t0, -1
 
-       lb $t1, 0($t0)				#Checking if the first value is less that 8 because if it's greater then the output will be faulty
+       lb $t1, 0($t0)				
 
        blt $t1, 58, exit_loop
 
        divu $s2, $s0
 
-       mflo $a0						#Flows the low to $a0,  prints it then exit_loop run
+       mflo $a0						#Flows the low to $a0,  prints it then exit_loop runs
 
        li $v0, 1
 
@@ -141,11 +167,7 @@ less_than_ten:
 exit_loop:
 
        li $v0, 1
-
        addi $a0, $s2, 0
-
        syscall
-
        li $v0, 10                 	#code to exit program
-
        syscall
